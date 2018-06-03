b5af2ac79434f1105899ce064a352d858c2355d1
ashutoshdhondkar <33003925+ashutoshdhondkar@users.noreply.github.com>
2018-06-03 11:42:20 +0530

    under_process

--- final_project.py
+++ final_project.py
@@ -1,5 +1,5 @@
-import xlsxwriter
-import xlrd
+from openpyxl import load_workbook
+from openpyxl.chart import PieChart3D,Reference,BarChart3D
 import re
 from bs4 import *
 import urllib.request
@@ -8,20 +8,20 @@ class SEO:
         self.wbk = wbk
         self.wsht = wsht
     def read_sheet_data(self):
-        col_no = int(input("Enter column number where data is stored : "))
-        col_no = col_no -1
-        if(col_no < 0):
-            print("Enter valid column number!")
-        elif(col_no >= 0):
-            self.data = self.wsht.col_values(col_no)
-            #removing empty elements from list
-            self.data = [x.lower() for x in self.data if(x)]
-            del self.wbk,self.wsht
-            if(len(self.data)==0):
-                return False
-            else:
-                print(self.data)
-                return True
+        self.data=[]
+        col_no =input("Enter column name : ")
+        try:
+            temp = wsht[col_no]
+            for x in temp:
+                if(x!=None):
+                    self.data.append(x.value.lower())
+        except Exception:
+            print("please specify correct column name ")
+        print(self.data)
+        if(len(self.data)>0):
+            return True
+        else:
+            return False
     def read_url_from_data(self):
         pattern = re.compile('(https://)|(http://)')
         for x in self.data:
@@ -29,60 +29,82 @@ class SEO:
                 self.url = x
                 self.data.remove(x)
                 return True
-                break
         return False
-    
     def web_scrapping(self):
         req = urllib.request.Request(self.url, data=None, 
              headers={ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36' } 
                                       )
-        req = urllib.request.urlopen(req)
-        soup = BeautifulSoup(req,'lxml')
-        for x in soup(['script','style']):
-            x.extract()
-        soup = soup.getText()
-        urlConts=[]
-        self.urlContents=[]
-        for x in soup.splitlines(): #split('\n')
-            if(len(x)>0):
-                x=x.lower()
-                urlConts.append(x.strip())
-        for x in urlConts:
-            self.urlContents.extend(x.split())
-        print(self.urlContents)
+        try:
+            req = urllib.request.urlopen(req)
+        except Exception:
+            return False
+        else:
+            soup = BeautifulSoup(req,'lxml')
+            for x in soup(['script','style']):
+                x.extract()
+            soup = soup.getText()
+            urlConts=[]
+            self.urlContents=[]
+            for x in soup.splitlines(): #split('\n')
+                if(len(x)>0):
+                    x=x.lower()
+                    urlConts.append(x.strip())
+            for x in urlConts:
+                self.urlContents.extend(x.split())
+            print(self.urlContents)
+            return True
     def CountFrequency(self):
-        wbk = xlsxwriter.Workbook('RESULT.xlsx')
-        wsht = wbk.add_worksheet('new')
-        count=[y for y in map((lambda y:self.urlContents.count(y)),self.data)]
-        i=0
-        for x in count:
-            wsht.write(i,1,x)
-            i=i+1
-        i=0
+        self.count =[y for y in map((lambda y:self.urlContents.count(y)),self.data)]
+        dict ={x:y for x,y in zip(self.data,self.count)}
+        print(dict)
+    def WriteFrequency(self,book):
+        self.wsht['C1'] = 'Words'
+        self.wsht['D1'] = 'Frequency'
+        i=2
         for x in self.data:
-            wsht.write(i,0,x)
-            i=i+1
-        count ={x:y for x,y in zip(self.data,count)}
-        print(count)
-        
+                self.wsht.cell(row = i,column = 3).value = x
+                i=i+1
+        i=2
+        for y in self.count:
+                self.wsht.cell(row = i,column = 4).value = y
+                i=i+1
+        self.wbk.save(book)
+    def DrawChart(self,book):
+        words = Reference(self.wsht,min_col=3,min_row=2,max_row=len(self.data)+1)
+        freq = Reference(self.wsht,min_col=4,min_row=2,max_row=len(self.data)+1)
+        print("\n"*5)
+        print(words)
+        print(freq)
+        print("\n"*5)
+        pie = PieChart3D()
+        pie.add_data(freq,titles_from_data=True)
+        pie.set_categories(words)
+        pie.title="Word Frequency in given URL"
+        self.wsht.add_chart(pie,'G1')
+        self.wbk.save(book)
 wbk = input("Enter location of workbook : ")
+book=wbk
 try:
-    wbk = xlrd.open_workbook(wbk)
+    wbk = load_workbook(wbk)
 except Exception:
     print("Sorry unable to locate the file specified")
     print("Hint: use extension (eg .xlsx)")
 else:
     wsht = input("Enter worksheet name : ")
     try:
-        wsht = wbk.sheet_by_name(wsht)
+        wsht = wbk[wsht]
     except Exception:
         print("Please enter valid worksheet name")
     else:
         seo = SEO(wbk,wsht)
         if(seo.read_sheet_data()):
             if(seo.read_url_from_data()):
-                seo.web_scrapping()
-                seo.CountFrequency()
+                if(seo.web_scrapping()):
+                    seo.CountFrequency()
+                    seo.WriteFrequency(book)
+                    seo.DrawChart(book)
+                else:
+                    print("Sorry we are unable to connect!")
             else:
                 print("No url found")
             #del wbk,wsht
