b7902ed8e386762667332d71eec201d899d74f49
ashutoshdhondkar <ashutoshdhondkar>
2018-09-22 05:46:33 +0530

    deleted

--- python_quiz/quiz/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-	path('login/',views.index,name='index'),
-	path('',views.Register,name='register'),
-	path('Home/',views.Login,name='check'),
-]
